88273ae259878634f7400383e3f40f71ff0dc06f
Ankit <krankit0777@gmail.com>
2025-06-19 10:40:05 +0530

    Task 1 Completed

---
+++ Week1_Engineering_fundamentals/Design_and_System_Pattern/1. Implementing the Singleton Pattern/Code/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger obj1=Logger.getInstance();
+        Logger obj2=Logger.getInstance();
+        Logger obj3=Logger.getInstance();
+        System.out.println(obj1.hashCode());
+        System.out.println(obj2.hashCode());
+        System.out.println(obj3.hashCode());
+    }
+}
--- Week1_Engineering_fundamentals/Design_and_System_Pattern/SingeltonPattern/Logger.java
+++
@@ -1,5 +0,0 @@
-class Logger{
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
